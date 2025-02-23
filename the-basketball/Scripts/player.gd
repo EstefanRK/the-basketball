92c80df2c1b581473b6c235c50406ec8849a7665
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2025-02-23 18:46:26 +0100

    Basketball game working

--- the-basketball/Scripts/player.gd
+++ the-basketball/Scripts/player.gd
@@ -1,21 +1,33 @@
 extends CharacterBody3D
 
-
+# CONSTANTS
 const SPEED = 2.5
 const JUMP_VELOCITY = 4.5
 
+# VARIABLES
+@onready var ball = $"../Basketball"
+@onready var ray = $Head/RayCast3D
+var item_picked_up = false
+
+#INTERACT
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+	if event.is_action_pressed("click"):
+		if item_picked_up:
+			ball.pickup()
+		if ray.is_colliding():
+			print("hit")
+			var hit = ray.get_collider()
+			if hit.has_method("pickup"):
+				ball.pickup()
+				print(item_picked_up)
 
+# MOVEMENT
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
