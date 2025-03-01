c66e77ae1c2b2d183966b01963ca5cec2c5c335f
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2025-03-01 17:28:35 +0100

    added controller support

--- The Orphan/Scripts/camera.gd
+++ The Orphan/Scripts/camera.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 const mouse_sensitivity = 0.005
+const controller_sensitivity = 2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -10,3 +11,12 @@ func _input(event: InputEvent) -> void:
 		get_parent().rotate_y(-event.relative.x*mouse_sensitivity)
 		rotate_x(-event.relative.y*mouse_sensitivity)
 		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		
+
+func _process(delta):
+	var cam_input_dir := Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	if cam_input_dir.length() > 0.02:  # Prevents tiny accidental movement
+		rotate_x(-cam_input_dir.y * controller_sensitivity * delta)
+		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+		get_parent().rotate_y(-cam_input_dir.x * controller_sensitivity * delta)
--- The Orphan/Scripts/main_menu.gd
+++ The Orphan/Scripts/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("enter"):
+		get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
